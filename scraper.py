ad97bbdf89c829563247d9ec151f201ff3839bb8
bdoln <bddolan@gmail.com>
2017-05-05 16:44:21 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,14 +17,14 @@ def scrape_table(root):
         # Set up our data record - we'll need it later
         record = {}
         table_cells = row.cssselect("td") if table_cells:
-          record['Artist'] = table_cells[0].text 
-          record['Album'] = table_cells[1].text 
-          record['Released'] = table_cells[2].text 
-          record['Sales m'] = table_cells[4].text 
-          # Print out the data we've gathered 
-          print record, '------------'
-          # Finally, save the record to the datastore - 'Artist' is our unique key
-          scraperwiki.sqlite.save(["Artist"], record)
+            record['Artist'] = table_cells[0].text 
+            record['Album'] = table_cells[1].text 
+            record['Released'] = table_cells[2].text 
+            record['Sales m'] = table_cells[4].text 
+            # Print out the data we've gathered 
+            print record, '------------'
+            # Finally, save the record to the datastore - 'Artist' is our unique key
+            scraperwiki.sqlite.save(["Artist"], record)
           
 #scrape_and_look_for_next_linkfunction:callsthescrape_table
 #function,thenhuntsfora'next'link:ifoneisfound,callsitselfagain
